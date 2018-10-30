98b3f4550cf97fc3e5e4e28e16ea8f2d8415edcc
liangjialin <liangjialin@tbkt.cn>
2018-10-30 17:38:16 +0800

    手术信息二级页面title布局OK

--- app/src/main/java/com/pda/pda_android/activity/apps/detail/SSXXInfomationActivity.java
+++ app/src/main/java/com/pda/pda_android/activity/apps/detail/SSXXInfomationActivity.java
@@ -1,22 +1,27 @@
 package com.pda.pda_android.activity.apps.detail;
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Parcelable;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.pda.pda_android.R;
+import com.pda.pda_android.activity.UsersListActivity;
 import com.pda.pda_android.base.BaseActivity;
 import com.pda.pda_android.base.utils.LogUtils;
 import com.pda.pda_android.bean.UserBean;
-
-import java.io.Serializable;
 
 /**
  * 梁佳霖创建于：2018/10/30 15:28
  * 功能：手术信息二级页面
  */
 public class SSXXInfomationActivity extends BaseActivity {
+
+    private ImageView title_back;
+    private ImageView user_name_up;
+    private ImageView users_all;
+    private ImageView user_name_down;
+    private TextView user_info;
+
     @Override
     public int setLayoutId() {
         return R.layout.activity_ssxxinfomation;
@@ -25,7 +30,24 @@ public class SSXXInfomationActivity extends BaseActivity {
     @Override
     public void initView() {
         UserBean userBean = (UserBean) getIntent().getSerializableExtra("userBean");
-        LogUtils.showLog("5555555",userBean.toString());
+        title_back = findViewById(R.id.title_back);
+        title_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SSXXInfomationActivity.this.finish();
+            }
+        });
+        user_name_up = findViewById(R.id.user_name_up);
+        user_name_down = findViewById(R.id.user_name_down);
+        users_all = findViewById(R.id.users_all);
+        users_all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SSXXInfomationActivity.this.finish();
+            }
+        });
+        user_info = findViewById(R.id.user_info);
+        user_info.setText(userBean.getBed_no()+"  "+userBean.getPatient_name());
     }
 
     @Override
