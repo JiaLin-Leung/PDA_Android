ce515c0292ade6c7015edcdeaa8eb3f50a2cf39a
liangjialin <liangjialin@tbkt.cn>
2018-11-02 15:42:49 +0800

    修改数据库存储

--- app/src/main/java/com/pda/pda_android/db/converter/CheckBeanListBean_Converter.java
+++ app/src/main/java/com/pda/pda_android/db/converter/CheckBeanListBean_Converter.java
@@ -1,5 +1,6 @@
 package com.pda.pda_android.db.converter;
 
+import com.alibaba.fastjson.JSON;
 import com.google.gson.Gson;
 import com.pda.pda_android.db.Entry.CheckBeanListBean;
 
@@ -16,30 +17,11 @@ import java.util.List;
 public class CheckBeanListBean_Converter implements PropertyConverter<List<CheckBeanListBean>, String> {
     @Override
     public List<CheckBeanListBean> convertToEntityProperty(String databaseValue) {
-        if (databaseValue == null) {
-            return null;
-        }
-        List<String> list_str = Arrays.asList(databaseValue.split(","));
-        List<CheckBeanListBean> list_transport = new ArrayList<>();
-        for (String s : list_str) {
-            list_transport.add(new Gson().fromJson(s, CheckBeanListBean.class));
-        }
-        return list_transport;
+        return JSON.parseArray(databaseValue, CheckBeanListBean.class);
     }
 
     @Override
     public String convertToDatabaseValue(List<CheckBeanListBean> arrays) {
-        if (arrays == null) {
-            return null;
-        } else {
-            StringBuilder sb = new StringBuilder();
-            for (CheckBeanListBean array : arrays) {
-                String str = new Gson().toJson(array);
-                sb.append(str);
-                sb.append(",");
-            }
-            return sb.toString();
-
-        }
+        return JSON.toJSONString(arrays);
     }
 }
